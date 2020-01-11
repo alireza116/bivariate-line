1b1786b302621ab2410c95fd1a9190f6c2f84540
darts <darts@dArtss-MacBookPro.local>
2020-01-11 16:29:04 -0500

    added alpha value

--- js/bootstrap_stat.js
+++ js/bootstrap_stat.js
@@ -16,18 +16,18 @@ function bootstrap_stat() {
     return bsResult;
   };
 
-  this.boot_ci_mean = function(data) {
+  this.boot_ci_mean = function(data, alpha = 0.05) {
     //requires data to be numerical;
     var bootstrap = this.bootstrap(data);
     var means = bootstrap.map(function(b) {
       return jStat.mean(b);
     });
     var mean = jStat.mean(means);
-    var CI = jStat.quantiles(means, [0.025, 0.975]);
+    var CI = jStat.quantiles(means, [0 + alpha / 2, 1 - alpha / 2]);
     return { mean: mean, CI: CI };
   };
 
-  this.boot_ci_pearsonr = function(data) {
+  this.boot_ci_pearsonr = function(data, alpha = 0.05) {
     var bootstrap = this.bootstrap(data);
     var pearsonRs = bootstrap.map(function(b) {
       var x = b.map(function(d) {
@@ -40,7 +40,7 @@ function bootstrap_stat() {
     });
 
     var pearsonr = jStat.mean(pearsonRs);
-    var CI = jStat.quantiles(pearsonRs, [0.025, 0.975]);
+    var CI = jStat.quantiles(pearsonRs, [0 + alpha / 2, 1 - alpha / 2]);
     return { pearsonr: pearsonr, CI: CI };
   };
 }
